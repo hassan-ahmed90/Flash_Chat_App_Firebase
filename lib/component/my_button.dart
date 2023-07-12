93fb4105143e5744c471b6a1d93e34b00438adbc
Hassan Ahmed <hassanahmed40005@gmail.com>
2023-07-12 22:22:49 +0500

    my commit

---
+++ lib/component/my_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: Colors.lightBlueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: () {
+
+            //Go to login screen.
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            'Log In',
+          ),
+        ),
+      ),
+    );
+  }
+}
